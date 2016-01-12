fa706378f1b82c059c1ba28c0ff5eaefe14c4f4c
programator <programator@192.168.99.230>
2016-01-12 10:08:18 +0100

    Zacatek komentaru

--- AdminModule/presenters/SignPresenter.php
+++ AdminModule/presenters/SignPresenter.php
@@ -6,18 +6,15 @@ use Nette;
 use App\Forms\SignFormFactory;
 
 
-class SignPresenter extends \App\Presenters\BasePresenter
-{
+class SignPresenter extends \App\Presenters\BasePresenter {
 	/** @var SignFormFactory @inject */
 	public $factory;
-
 
 	/**
 	 * Sign-in form factory.
 	 * @return Nette\Application\UI\Form
 	 */
-	protected function createComponentSignInForm()
-	{
+	protected function createComponentSignInForm() {
 		$form = $this->factory->create();
 		$form->onSuccess[] = function ($form) {
 			$form->getPresenter()->redirect('Dashboard:');
@@ -26,8 +23,7 @@ class SignPresenter extends \App\Presenters\BasePresenter
 	}
 
 
-	public function actionOut()
-	{
+	public function actionOut() {
 		$this->getUser()->logout();
 		$this->flashMessage('You have been signed out.');
 		$this->redirect('in');
