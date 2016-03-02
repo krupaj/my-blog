08d7dcbf2dea550b0c586e3b53dff773d9e5d46b
programator <programator@192.168.99.230>
2016-03-02 11:06:38 +0100

    Nastaveni TExy

--- FrontModule/presenters/BaseFrontPresenter.php
+++ FrontModule/presenters/BaseFrontPresenter.php
@@ -14,9 +14,16 @@ abstract class BaseFrontPresenter extends Presenters\BasePresenter {
 	
 	/** @var Model\Repository\SectionRepository @inject */
 	public $sectionRepository;
+	
+	 /** @var \Texy\Texy @inject */
+	public $texy;
 
 
 	public function beforeRender() {
+		//nastaveni spravne cesty k obrazkum pro Texy
+		//$this->texy->imageModule->root = $this->template->basePath . '/images/'; 
+		$this->texy->imageModule->root = '/images/'; 
+		
 		$this->template->today = new \Nette\Utils\DateTime();
 		$this->template->sections = $this->sectionRepository->getAllSections();
 	}
